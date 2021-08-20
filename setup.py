408ee722fa7e3731da1d62f6a9feb32baf91651f
Niels Vaes <nielsvaes@gmail.com>
2021-08-20 14:53:57 +0200

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='ez_qt',
-    version='1.1.0',
+    version='1.1.2',
     packages=['ez_qt'],
     url='https://www.github.com/nielsvaes/ez_qt',
     long_description=long_description,
